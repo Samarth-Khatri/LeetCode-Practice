25a0b0e074d4d553cfae7b2f7fa0ac1744914c6f
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-05-29 17:49:48 +0530

    Time: 0 ms (100.00%), Space: 40 MB (94.52%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -10,12 +10,5 @@ class Solution {
                 longPrefix = longPrefix.substring(0,longPrefix.length()-1);
         }
         return longPrefix;
-        
-        // Approach 2
-        // String ans = strs[0];
-        // for(int i=1;i<strs.length;i++)
-        //     while(strs[i].indexOf(ans)!=0)
-        //         ans = ans.substring(0,ans.length()-1);
-        // return ans;
     }
 }
