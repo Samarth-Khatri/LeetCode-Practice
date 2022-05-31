0a7b6526531fcc010d25a5583ffa1e6b35253aac
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-05-31 23:45:25 +0530

    Time: 589 ms (7.08%), Space: 42 MB (12.61%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.java
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -1,10 +1,44 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        // LOL
-        int n;
-        n=dividend/divisor;
-        if(dividend==Integer.MIN_VALUE&&divisor==-1)
-            return Integer.MAX_VALUE;
-        return n;
+    
+        long res = solve(dividend, divisor);
+        
+        return res > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int)res;
+    }
+    
+    long solve(long dividend, long divisor){
+        
+        if(dividend < 0 || divisor < 0){
+            
+            if(dividend < 0 && divisor < 0)
+                return  solve(Math.abs(dividend),  Math.abs(divisor));
+            
+            long ans = solve(Math.abs(dividend),  Math.abs(divisor));
+            return 0 - ans;
+        }
+        
+        if(dividend < divisor)
+            return 0;
+        
+        if(divisor == 1)
+            return dividend;
+        
+        long multiples = 1;
+        long sum = divisor;
+        
+        while(sum < dividend){
+            multiples += multiples;
+            sum += sum;
+        }
+        
+        if(sum == dividend){
+            return multiples;
+        }
+        
+        while(sum > dividend){
+            sum -= divisor;
+            multiples -= 1;
+        }
+        return multiples;
     }
 }
