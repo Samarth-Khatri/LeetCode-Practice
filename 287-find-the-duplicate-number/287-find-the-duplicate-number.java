71dc47db33f925b867a6a227fdd26894991aeeeb
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-06-03 21:34:20 +0530

    Update 287-find-the-duplicate-number.java

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,12 +1,13 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // Arrays.sort(nums);
-        // int ans=0;
-        // for(int i=0;i<nums.length-1;++i) 
-        //     if(nums[i]==nums[i+1])
-        //         ans=nums[i+1];
-        // return ans;
+        Arrays.sort(nums);
+        int ans=0;
+        for(int i=0;i<nums.length-1;++i) 
+            if(nums[i]==nums[i+1])
+                ans=nums[i+1];
+        return ans;
         
+        // Faster Approach
         int len = nums.length;
         int[] cnt = new int[len + 1];
         for (int i = 0; i < len; i++) {
