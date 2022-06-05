88bd887ee21e8067fb6e814ba076bcd81d5e4a77
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-06-05 18:41:35 +0530

    Time: 1 ms (98.63%), Space: 39.7 MB (78.76%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,25 +1,25 @@
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        int ans = 1;
-        for(int i=0;i<=30;i++){
-            if(ans == n)
-                return true;
-            if(ans < Integer.MAX_VALUE/2)
-            ans*=2;
-        }
-        return false;
-    }
-}
 // class Solution {
-// 	public boolean isPowerOfTwo(int n) {
-// 		if(n==1)
-//             return true; 
-// 		if(n==0)
-//             return false; 
-// 		if(n%2!=0)
-//             return false; 
-// 		else
-//             return isPowerOfTwo(n/2); 
-
-// 	}
+//     public boolean isPowerOfTwo(int n) {
+//         int ans = 1;
+//         for(int i=0;i<=30;i++){
+//             if(ans == n)
+//                 return true;
+//             if(ans < Integer.MAX_VALUE/2)
+//             ans*=2;
+//         }
+//         return false;
+//     }
 // }
+class Solution {
+	public boolean isPowerOfTwo(int n) {
+		if(n==1)
+            return true; 
+		if(n==0)
+            return false; 
+		if(n%2!=0)
+            return false; 
+		else
+            return isPowerOfTwo(n/2); 
+
+	}
+}
