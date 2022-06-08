73285a0b08934f327dc93efb013d1d6f004857fc
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-06-08 18:49:47 +0530

    Added solution - LeetHub

---
+++ Find the median - GFG/find-the-median.java
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v  = new int[n];
+            for(int  i = 0; i < n; i++)
+                v[i] = Integer.parseInt(S[i]);
+            Solution ob = new Solution();
+            int ans = ob.find_median(v);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int find_median(int[] v)
+    {
+        Arrays.sort(v);
+        if(v.length%2==0) {
+            int med = (v[v.length/2] + v[v.length/2 -1])/2;
+            return med;
+        }
+        else
+            return v[v.length/2];
+    }
+}
