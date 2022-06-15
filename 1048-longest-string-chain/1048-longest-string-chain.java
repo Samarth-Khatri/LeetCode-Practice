c8fc52cb8eeb1206107fdb518b7af627093437c2
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-06-16 03:55:40 +0530

    Time: 106 ms (13.80%), Space: 45.5 MB (68.65%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words,(a,b)->{
+            return a.length()-b.length();
+        });
+        int omax=0;
+        int dp[]=new int[words.length];
+        for(int i=0;i<words.length;++i) {
+            int max=0;
+            for(int j=0;j<i;++j) 
+                if(CheckPossible(words[i],words[j]))
+                    max=Math.max(max,dp[j]);
+            dp[i]=max+1;
+            if(omax<dp[i]) 
+                omax=dp[i];
+        }
+        return omax;
+     }
+    
+    public static boolean CheckPossible(String w1,String w2) {
+        if(w1.length()!=w2.length()+1) 
+            return false;
+        int first=0; 
+        int second=0;
+        while(first<w1.length()) {
+            if(second<w2.length() && w1.charAt(first)==w2.charAt(second)){
+                first++;
+                second++;
+            }
+            else 
+                first++;
+        }
+        if(first==w1.length() && second==w2.length()) 
+            return true;
+        else 
+            return false;
+    }
+}
