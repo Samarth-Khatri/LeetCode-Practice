56fa62a8136f1a7932cbdafc0f9997e803c3d143
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-06-19 18:11:48 +0530

    Time: 42 ms (63.56%), Space: 59.4 MB (67.22%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -3,15 +3,14 @@ class Solution {
         List<List<String>> ans = new ArrayList();
         Arrays.sort(products);
         String subs="";
-        for(int i=0;i<searchWord.length();++i){
+        for(int i=0;i<searchWord.length();++i) {
             List<String> ians = new ArrayList();
             subs += searchWord.charAt(i);
-            for(int j=0;j<products.length;++j){
-                if(products[j].startsWith(subs)){
+            for(int j=0;j<products.length;++j) {
+                if(products[j].startsWith(subs)) {
                     ians.add(products[j]);
-                    if(ians.size()==3){
+                    if(ians.size()==3)
                       break;
-                     }
                 }
             }
             ans.add(ians);
