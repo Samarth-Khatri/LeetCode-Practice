9fec29a00aee19e37ca9e178e942bba82559b4d4
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-06-21 03:15:30 +0530

    Time: 461 ms (10.36%), Space: 46.3 MB (87.80%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minimumLengthEncoding(String[] words) {
         Arrays.sort(words,(a,b)->Integer.compare(b.length(),a.length()));
-        int ans=0;
+        int ans = 0;
         for(int i=0;i<words.length;++i) {
             boolean f = false;
             for(int j=0;j<=i-1;++j) {
