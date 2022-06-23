3670e6cad9accf64b90d696c1e5068a8b8c3dc95
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-06-24 03:16:13 +0530

    Time: 77 ms (22.10%), Space: 71 MB (50.09%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+  public int scheduleCourse(int[][] courses) {
+    Arrays.sort(courses, (int[] o1, int[] o2) -> o1[1] - o2[1]);
+    
+    int total_days = 0;
+    PriorityQueue<Integer> q = new PriorityQueue<>(courses.length,(o1, o2) -> o2 - o1);
+    
+    for(int[] it: courses){ 
+        q.add(it[0]);
+        total_days += it[0];
+        if (total_days > it[1]) 
+            total_days -= q.poll();
+    }
+    
+    return q.size();
+  
+  }
+    
+}
