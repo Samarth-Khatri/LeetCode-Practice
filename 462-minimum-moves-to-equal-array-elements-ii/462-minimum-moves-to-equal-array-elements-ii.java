9f489aa2942b531ffc665a6b286eae9c1c8dd2a6
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-06-30 10:59:55 +0530

    Time: 5 ms (66.60%), Space: 45.6 MB (56.42%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -3,7 +3,8 @@ class Solution {
         Arrays.sort(nums); // sort the array
         
         // find the median
-        int median = nums.length%2==0 ? (nums[nums.length/2]+nums[nums.length/2-1])/2 : nums[nums.length/2];
+        int n = nums.length;
+        int median = n%2==0 ? (nums[n/2]+nums[n/2-1])/2 : nums[n/2];
         
         // calculating steps
         int steps=0;
