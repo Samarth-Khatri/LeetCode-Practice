07e555b1492ea54afdcbc7756023a494773ef5bd
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-06-30 11:21:18 +0530

    Update 462-minimum-moves-to-equal-array-elements-ii.java

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,13 +1,13 @@
 class Solution {
     public int minMoves2(int[] nums) {
         // Approach 1
-        // Arrays.sort(nums); // sort the array
-        // int n = nums.length; // find the median
-        // int median = n%2==0 ? (nums[n/2]+nums[n/2-1])/2 : nums[n/2];
-        // int steps=0; // calculating steps
-        // for(int num : nums)
-        //     steps += Math.abs(num-median);
-        // return steps;
+        Arrays.sort(nums); // sort the array
+        int n = nums.length; // find the median
+        int median = n%2==0 ? (nums[n/2]+nums[n/2-1])/2 : nums[n/2];
+        int steps=0; // calculating steps
+        for(int num : nums)
+            steps += Math.abs(num-median);
+        return steps;
         
         // Aproach 2
         Arrays.sort(nums); // sort the array 
