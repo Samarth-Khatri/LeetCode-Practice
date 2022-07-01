6e715ea11b176e1ab4fd2d984193de6651d0767d
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-01 11:48:12 +0530

    Added solution - LeetHub

---
+++ Merge two sorted linked lists - GFG/merge-two-sorted-linked-lists.java
@@ -0,0 +1,125 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+
+
+class MergeLists
+{
+    Node head;
+
+
+
+  /* Function to print linked list */
+   public static void printList(Node head)
+    {
+        
+        while (head!= null)
+        {
+           System.out.print(head.data+" ");
+           head = head.next;
+        }  
+        System.out.println();
+    }
+	
+	 
+ 
+     /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+       
+         
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			Node head1 = new Node(sc.nextInt());
+            Node tail1 = head1;
+            for(int i=0; i<n1-1; i++)
+            {
+                tail1.next = new Node(sc.nextInt());
+                tail1 = tail1.next;
+            }
+			Node head2 = new Node(sc.nextInt());
+            Node tail2 = head2;
+            for(int i=0; i<n2-1; i++)
+            {
+                tail2.next = new Node(sc.nextInt());
+                tail2 = tail2.next;
+            }
+			
+			LinkedList obj = new LinkedList();
+			Node head = obj.sortedMerge(head1,head2);
+			printList(head);
+			
+			t--;
+			
+         }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+  Merge two linked lists 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class LinkedList
+{
+    //Function to merge two sorted linked list.
+    Node sortedMerge(Node head1, Node head2) {
+     // This is a "method-only" submission. 
+     // You only need to complete this method
+        Node head,tail,cur1=head1,cur2=head2;
+        if(head1.data<=head2.data) {
+            head=head1;
+            tail=head1;
+            cur1=cur1.next;
+        }
+        else {
+            head=head2;
+            tail=head2;
+            cur2=cur2.next;
+        }
+        while(cur1!=null&&cur2!=null) {
+            if(cur1.data<=cur2.data) {
+                tail.next=cur1;
+                tail=cur1;
+                cur1=cur1.next;
+            }
+            else {
+                tail.next=cur2;
+                tail=cur2;
+                cur2=cur2.next;
+            }
+        }
+        if(cur1!=null)
+            tail.next=cur1;
+        
+        else if(cur2!=null)
+            tail.next=cur2;
+            
+        return  head;
+   } 
+}
