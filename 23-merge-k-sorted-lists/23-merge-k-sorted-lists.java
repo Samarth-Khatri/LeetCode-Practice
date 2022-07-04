c56ecb602aaa1742fd01fe0936cabcd6cdb479c7
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-04 17:04:05 +0530

    Time: 105 ms (21.48%), Space: 44.8 MB (82.79%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0) 
+            return null;
+        ListNode ans = null; // new list 
+        for(ListNode head:lists) // iterating through list array
+            ans = mergeTwoLists(ans,head); // merging ans list and next list in array
+        return ans;
+    }
+    
+     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null || list2==null) // null condition check
+            return list1!=null ? list1 : list2;
+        
+        ListNode c1 = list1, c2 = list2, dummy = new ListNode(-1), prev = dummy;
+        
+        while(c1!=null && c2!=null) { // until 1 list is null
+            if(c1.val<c2.val) { // attach c1 if c1 is less
+                prev.next = c1;
+                c1 = c1.next;
+            } 
+            else {  // attach c2 if c2 is less
+                prev.next = c2;
+                c2 = c2.next;
+            }
+            prev = prev.next; // move prev to next
+        }
+        
+        prev.next = (c1!=null ? c1 : c2); // attach prev to the list which is not null
+        
+        ListNode head = dummy.next;
+        dummy.next = null;
+        return head;
+    }
+}
