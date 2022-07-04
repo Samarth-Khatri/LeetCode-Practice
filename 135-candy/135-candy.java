6761ab1aa5a7a1236b535d1d4f48c96d831a83e0
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-04 10:29:36 +0530

    Time: 6 ms (29.60%), Space: 52.1 MB (32.98%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -10,12 +10,12 @@ class Solution {
         // Left relative array
         for(int i=1;i<n;++i)
             if(ratings[i]>ratings[i-1])
-                left[i]+=left[i-1];
+                left[i]=left[i-1]+1;
         
         // right relative array
         for(int i=n-2;i>=0;--i)
             if(ratings[i]>ratings[i+1])
-                right[i]+=right[i+1];
+                right[i]=right[i+1]+1;
         
         // Merge both sides
         for(int i=0;i<n;++i)
