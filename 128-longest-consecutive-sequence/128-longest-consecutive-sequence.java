1854e498656b7dd0b369b09d3be4d6392643470e
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-05 10:59:35 +0530

    Time: 23 ms (87.86%), Space: 61.3 MB (82.38%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,25 +1,42 @@
 class Solution {
     
     // Approach 1 -> Using Sorting
-    public int longestConsecutive(int[] nums) {
-        Arrays.sort(nums);
-        if(nums.length<2) 
-            return nums.length;
-        int cnt=1, lcs=1;
-        for(int i=1;i<nums.length;++i) {
-            if(nums[i-1]==nums[i]) 
-                continue;
-            else if(nums[i-1]+1 == nums[i]) 
-                cnt++;
-            else 
-                cnt=1;
+//     public int longestConsecutive(int[] nums) {
+//         Arrays.sort(nums); // sorting
+//         if(nums.length<2) 
+//             return nums.length;
+//         int cnt=1, lcs=1;
+//         for(int i=1;i<nums.length;++i) {
+//             if(nums[i-1]==nums[i])  // duplicate check
+//                 continue;
+//             else if(nums[i-1]+1 == nums[i]) // consecutive check
+//                 cnt++;
+//             else 
+//                 cnt=1;     // if consecutive sequence breaks reset count 
 
-            lcs = Math.max(lcs,cnt);
-        }
+//             lcs = Math.max(lcs,cnt); // max of count and prev longest consecutive subsequence
+//         }
         
-    return lcs;
+//     return lcs;
         
-    }
+//     }
     
     // Approach 2 -> Using HashSet
+    public int longestConsecutive(int[] nums) {
+        if(nums.length<2) 
+            return nums.length;
+        Set<Integer> hashSet = new HashSet<Integer>();
+        for(int num : nums)  // adding to hashset
+            hashSet.add(num);
+        int lcs=1;
+        for (int num: hashSet) 
+            if (!hashSet.contains(num-1)) {
+                int cnt=1;
+                // check if element is the start of a sequence: (element-1) does not exist in set
+                while (hashSet.contains(num+cnt)) 
+                    cnt++;
+                lcs = Math.max(lcs,cnt); // max of count and prev longest consecutive subsequence
+            }
+        return lcs;
+    }
 }
