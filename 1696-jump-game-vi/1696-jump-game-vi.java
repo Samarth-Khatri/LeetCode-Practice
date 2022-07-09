3d4c3475ff0b8e12f2ee5d504eed99f9918c80c8
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-09 22:34:16 +0530

    Time: 23 ms (96.96%), Space: 57.2 MB (92.92%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        
+        LinkedList<Integer> list = new LinkedList<>(); // for storing indices
+        list.add(0);
+        
+        for(int i=1;i<nums.length;++i){
+			
+            //checking if the value lies between the range of last k values
+            if(list.getFirst() < i-k) 
+                list.removeFirst();
+            
+			// setting nums[i] equal to curr. value and curr. max value
+            nums[i] += nums[list.getFirst()];
+			
+            //if index does not lie between last k values -> remove 
+            while(!list.isEmpty() && nums[list.getLast()] <= nums[i]) 
+                list.removeLast();
+            
+            list.addLast(i);
+        }
+        
+        return nums[nums.length-1];
+    }
+}
