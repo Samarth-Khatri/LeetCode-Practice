233faa468bca803b8f7d243f57604a03d11b4cf0
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-10 13:42:09 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (80.92%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,4 +1,5 @@
 class Solution {
+    // Iterative Solution
     public int tribonacci(int n) {
         int a=0,b=1,c=1;
         if(n==0)
@@ -16,4 +17,15 @@ class Solution {
         }
         return d;
     }
+    
+    // Recursive Solution -> TLE
+    // public int tribonacci(int n) {
+    //     if(n==0)
+    //         return 0;
+    //     if(n==1)
+    //         return 1;
+    //     if(n==2)
+    //         return 1;
+    //     return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+    // }
 }
