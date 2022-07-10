c3032e8ee1ff6b214f0c3b5cb2413115c51923b9
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-10 13:42:50 +0530

    Update 1137-n-th-tribonacci-number.java

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -19,13 +19,13 @@ class Solution {
     }
     
     // Recursive Solution -> TLE
-    // public int tribonacci(int n) {
-    //     if(n==0)
-    //         return 0;
-    //     if(n==1)
-    //         return 1;
-    //     if(n==2)
-    //         return 1;
-    //     return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
-    // }
+    public int tribonacci(int n) {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 1;
+        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+    }
 }
