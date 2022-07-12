8ee7e90d717b43e29039d2cd3b5e1739a9d02f26
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-13 03:05:32 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.java
+++ Kth smallest element - GFG/kth-smallest-element.java
@@ -33,10 +33,40 @@ class GFG {
 //User function Template for Java
 
 class Solution{
-    public static int kthSmallest(int[] arr, int l, int r, int k) 
-    { 
-        //Your code here
-        Arrays.sort(arr);
-        return arr[k-1];
+    
+    // QuickSelect -> TLE :(
+    public static int kthSmallest(int[] arr, int l, int r, int k) { 
+          return quickSelect(arr,l,r,k-1);
     } 
+    
+    public static int quickSelect(int[] arr, int lo, int hi, int k) {
+        int pivot = arr[hi];
+        int pidx = partition(arr,pivot,lo,hi);
+        if(pidx==k)
+          return pivot;
+        else if(k>pidx)
+          return quickSelect(arr,pidx+1,hi,k);
+        else
+          return quickSelect(arr,lo,pidx-1,k);
+    }
+  
+    public static int partition(int[] arr, int pivot, int lo, int hi) {
+        int i = lo, j = lo;
+        while (i <= hi) {
+            if (arr[i] <= pivot) {
+                swap(arr, i, j);
+                i++;
+                j++;
+            }
+            else 
+                i++;
+        }
+        return (j - 1);
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
