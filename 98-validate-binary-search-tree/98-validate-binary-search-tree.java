7dc7baf5cb4fe2a757eaa75387c194b20c9c9fa8
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-21 21:02:26 +0530

    Update 98-validate-binary-search-tree.java

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -15,20 +15,20 @@
  */
 
 // Method 1
-// class Solution {
+class Solution {
 
-//     public boolean isValidBST(TreeNode root) {
-//         return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
-//     }
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
 
-//     public boolean isValid(TreeNode root, long min, long max) {
-//         if (root == null)
-//             return true;
-//         if (root.val <= min || root.val >= max)
-//             return false;
-//         return (isValid(root.left, min, root.val) && isValid(root.right, root.val, max));
-//     }
-// }
+    public boolean isValid(TreeNode root, long min, long max) {
+        if (root == null)
+            return true;
+        if (root.val <= min || root.val >= max)
+            return false;
+        return (isValid(root.left, min, root.val) && isValid(root.right, root.val, max));
+    }
+}
 
 // Method 2
 class Solution {
