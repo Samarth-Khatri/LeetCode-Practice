c7f5faad33ebb78975c8a888d0f89a84711da136
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-24 22:17:42 +0530

    Time: 0 ms (100.00%), Space: 50.2 MB (11.72%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -16,39 +16,39 @@
 class Solution {
 
     public TreeNode deleteNode(TreeNode node, int key) {
-        if (node == null) 
+        if (node == null) // base case
             return null;
 
         if (node.val > key) 
-            node.left = deleteNode(node.left, key);
+            node.left = deleteNode(node.left, key); // left call bst condition
         
-        else if (node.val < key) 
-            node.right = deleteNode(node.right, key); 
+        else if (node.val < key)  
+            node.right = deleteNode(node.right, key); // right call bst condition
         
         else {
             
-            if (node.left != null && node.right != null) {
-                int lmax = max(node.left);
-                node.val = lmax;
-                node.left = deleteNode(node.left, lmax);
+            if (node.left != null && node.right != null) { // both child present
+                int lmax = max(node.left);                 // find max from left
+                node.val = lmax;                           // replace data of node with max
+                node.left = deleteNode(node.left, lmax);   // remove max
                 return node;
             } 
             
-            else if (node.left != null) 
+            else if (node.left != null) // remove when only left child present
                 return node.left; 
             
-            else if (node.right != null) 
+            else if (node.right != null) // remove when only right child present
                 return node.right; 
             
             else 
-                return null;
+                return null; // leaf remove
         }
 
         return node;
     }
 
-    public int max(TreeNode node) {
-        if(node.right != null)
+    public int max(TreeNode node) { // returns max
+        if(node.right != null) 
             return max(node.right);
         
         else
