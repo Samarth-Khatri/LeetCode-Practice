642332c4e077aa834bc640a7c0c2622f5d8009d2
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-28 08:28:56 +0530

    Time: 16 ms (33.30%), Space: 47.5 MB (5.37%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,16 +1,44 @@
 class Solution {
+    
+    // Using Freq Array
+    // public boolean isAnagram(String s, String t) {
+    //     int[] alphabets=new int[26];
+    //     for (char c : s.toCharArray()) 
+    //         alphabets[c - 'a']++;
+    //     for (char c : t.toCharArray()) {
+    //         alphabets[c - 'a']--;
+    //         if (alphabets[c - 'a'] == -1) 
+    //             return false;
+    //     }
+    //     for (int a : alphabets) 
+    //         if (a!=0) 
+    //             return false;
+    //     return true;
+    // }
+    
+    // Using HashMap
     public boolean isAnagram(String s, String t) {
-        int[] alphabets=new int[26];
-        for (char c : s.toCharArray()) 
-            alphabets[c - 'a']++;
-        for (char c : t.toCharArray()) {
-            alphabets[c - 'a']--;
-            if (alphabets[c - 'a'] == -1) 
-                return false;
-        }
-        for (int a : alphabets) 
-            if (a!=0) 
-                return false;
-        return true;
+        HashMap<Character,Integer> hs = new HashMap<>(); // freq of each letter of the string S
+        HashMap<Character, Integer> h = new HashMap<>(); // freq of each letter fo the string T
+        if(s.length() > t.length())
+            return false;
+        
+        for(Character ch: s.toCharArray())
+            if(h.containsKey(ch))
+		        h.put(ch, h.get(ch) + 1); 
+            else
+                h.put(ch,1);
+        
+        for(Character c: t.toCharArray()) 
+            if(hs.containsKey(c))
+		        hs.put(c, hs.get(c) + 1); 
+            else
+                hs.put(c,1);
+        
+        if(h.equals(hs) == true) 
+            return true;
+
+        return false;
+        
     }
 }
