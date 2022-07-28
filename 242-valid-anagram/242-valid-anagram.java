299503afb0f299796afeb0bf7770ff3362b6de80
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-28 08:29:48 +0530

    Update 242-valid-anagram.java

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,20 +1,20 @@
 class Solution {
     
     // Using Freq Array
-    // public boolean isAnagram(String s, String t) {
-    //     int[] alphabets=new int[26];
-    //     for (char c : s.toCharArray()) 
-    //         alphabets[c - 'a']++;
-    //     for (char c : t.toCharArray()) {
-    //         alphabets[c - 'a']--;
-    //         if (alphabets[c - 'a'] == -1) 
-    //             return false;
-    //     }
-    //     for (int a : alphabets) 
-    //         if (a!=0) 
-    //             return false;
-    //     return true;
-    // }
+    public boolean isAnagram(String s, String t) {
+        int[] alphabets=new int[26];
+        for (char c : s.toCharArray()) 
+            alphabets[c - 'a']++;
+        for (char c : t.toCharArray()) {
+            alphabets[c - 'a']--;
+            if (alphabets[c - 'a'] == -1) 
+                return false;
+        }
+        for (int a : alphabets) 
+            if (a!=0) 
+                return false;
+        return true;
+    }
     
     // Using HashMap
     public boolean isAnagram(String s, String t) {
