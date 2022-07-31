86d6890e998fad99b47a61bc2e94fd8af0b35368
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-07-31 16:34:51 +0530

    Time: 183 ms (25.70%), Space: 128.1 MB (61.83%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,85 +1,90 @@
 class NumArray {
-    // Prefix Sum Approach -> TLE
-    //     private int[] prefixSum;
-    //     private int[] nums;
-    //     public NumArray(int[] nums) {
-    //         this.nums = nums;
-    //         prefixSum = new int[nums.length + 1];
-    //         for(int i=1;i<=nums.length;++i)
-    //             prefixSum[i] = nums[i-1] + prefixSum[i-1];
-    //     }
-
-    //     public void update(int index, int val) {
-    //         int prevVal = nums[index];
-    //         nums[index] = val;
-    //         for(int i=index+1;i<prefixSum.length;++i)
-    //             prefixSum[i] += val - prevVal;
-    //     }
-
-    //     public int sumRange(int left, int right) {
-    //         return prefixSum[right + 1] - prefixSum[left];
-    //     }
-
-    // Segment Trees
-    int tree[];
-    int nums[];
-    int len;
+    // Normal Approach
+    List<Integer> arr = new ArrayList<>();
+    int totalSum = 0;
 
     public NumArray(int[] nums) {
-        this.nums = nums;
-        len = nums.length;
-        tree = new int[4 * len];
-
-        build(1, 0, len - 1);
-    }
-
-    private void build(int node, int start, int end) {
-        if (start == end) {
-            tree[node] = nums[start];
-            return;
+        for (int i = 0; i < nums.length; i++) {
+            arr.add(nums[i]);
+            totalSum += nums[i];
         }
-
-        int mid = start + (end - start) / 2;
-        build(node * 2, start, mid);
-        build(node * 2 + 1, mid + 1, end);
-
-        tree[node] = tree[node * 2] + tree[node * 2 + 1];
     }
 
     public void update(int index, int val) {
-        update(1, 0, len - 1, index, val - nums[index]);
-        nums[index] = val;
-    }
-
-    private void update(int node, int start, int end, int index, int toAdd) {
-        if (start == end) {
-            tree[node] += toAdd;
-            return;
-        }
-
-        int mid = start + (end - start) / 2;
-        if (index <= mid) 
-            update(node * 2, start, mid, index, toAdd); 
-        else 
-            update(node * 2 + 1, mid + 1, end, index, toAdd);
-
-        tree[node] = tree[node * 2] + tree[node * 2 + 1];
+        totalSum = totalSum + val - arr.get(index);
+        arr.set(index, val);
     }
 
     public int sumRange(int left, int right) {
-        return query(1, 0, len - 1, left, right);
+        int sum = totalSum;
+        for (int i = 0; i < left; i++) 
+            sum = sum - arr.get(i);
+        for (int i = right + 1; i < arr.size(); i++) 
+            sum = sum - arr.get(i);
+        return sum;
     }
+    
+    // Segment Trees
+    //     int tree[];
+    //     int nums[];
+    //     int len;
 
-    private int query(int node, int start, int end, int left, int right) {
-        if (start > right || end < left) 
-            return 0;
-        if (start >= left && end <= right) 
-            return tree[node];
+    //     public NumArray(int[] nums) {
+    //         this.nums = nums;
+    //         len = nums.length;
+    //         tree = new int[4 * len];
 
-        int mid = start + (end - start) / 2;
-        return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
-    }
+    //         build(1, 0, len - 1);
+    //     }
+
+    //     private void build(int node, int start, int end) {
+    //         if (start == end) {
+    //             tree[node] = nums[start];
+    //             return;
+    //         }
+
+    //         int mid = start + (end - start) / 2;
+    //         build(node * 2, start, mid);
+    //         build(node * 2 + 1, mid + 1, end);
+
+    //         tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    //     }
+
+    //     public void update(int index, int val) {
+    //         update(1, 0, len - 1, index, val - nums[index]);
+    //         nums[index] = val;
+    //     }
+
+    //     private void update(int node, int start, int end, int index, int toAdd) {
+    //         if (start == end) {
+    //             tree[node] += toAdd;
+    //             return;
+    //         }
+
+    //         int mid = start + (end - start) / 2;
+    //         if (index <= mid)
+    //             update(node * 2, start, mid, index, toAdd);
+    //         else
+    //             update(node * 2 + 1, mid + 1, end, index, toAdd);
+
+    //         tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    //     }
+
+    //     public int sumRange(int left, int right) {
+    //         return query(1, 0, len - 1, left, right);
+    //     }
+
+    //     private int query(int node, int start, int end, int left, int right) {
+    //         if (start > right || end < left)
+    //             return 0;
+    //         if (start >= left && end <= right)
+    //             return tree[node];
+
+    //         int mid = start + (end - start) / 2;
+    //         return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
+    //     }
 }
+
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray obj = new NumArray(nums);
