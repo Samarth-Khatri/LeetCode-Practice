684f5499ee2ea8b8ced38cef39d27c86bea6a754
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-08-02 23:40:29 +0530

    Time: 33 ms (28.72%), Space: 56.7 MB (60.80%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<matrix.length;++i)
             for(int j=0;j<matrix.length;++j) 
                 pq.add(matrix[i][j]);
-        int ans = 0;
+        int ans = Integer.MAX_VALUE;
         for(int i=0;i<k;++i)
             ans = pq.remove();
         return ans;
