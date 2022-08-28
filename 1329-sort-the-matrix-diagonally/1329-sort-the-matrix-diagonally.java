068daef3e0fe4a7b51ae28477be9950998d7a30a
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-08-28 18:19:50 +0530

    Time: 1 ms (100.00%), Space: 47.9 MB (63.28%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -16,23 +16,25 @@ class Solution {
     }
 
     public void sort(int[][] mat, int row, int col, int m, int n) {
-        List<Integer> values = new ArrayList<>();
+        int[] values = new int[101];
         int r = row, c = col;
+        
         while (r < m && c < n) {
-            values.add(mat[r][c]);
+            values[mat[r][c]]++;
             r++;
             c++;
         }
 
-        Collections.sort(values);
+        r = row; c = col;
 
-        r = row;
-        c = col;
-        int ind = 0;
-        while (r < m && c < n) {
-            mat[r][c] = values.get(ind++);
-            r++;
-            c++;
-        }
+        for (int i = 1; i < 101; i++) 
+            if (values[i] > 0) {
+                int count = values[i];
+                while (count-- > 0) {
+                    mat[r][c] = i;
+                    r++;
+                    c++;
+                }
+            }
     }
 }
