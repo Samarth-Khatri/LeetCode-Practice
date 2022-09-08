ca082f2bb67deff32bcdbf7e0bc62d6bf7e62f69
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-09-08 13:47:47 +0530

    Time: 28 ms (58.13%), Space: 62.2 MB (76.77%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -24,14 +24,16 @@ class Solution {
         while (!pq.isEmpty()) {
             Pair rem = pq.remove();
             
-            if (visited[rem.vtx])  continue;
+            if(visited[rem.vtx])  
+                continue;
             count--;
-            if (count == 0)  return rem.wt;
+            if(count == 0)  
+                return rem.wt;
             visited[rem.vtx] = true;
             
-            for (int i = 0; i < times.length; ++i) 
-                if (times[i][0] - 1 == rem.vtx) 
-                    if (!visited[times[i][1] - 1]) 
+            for(int i = 0; i < times.length; ++i) 
+                if(times[i][0] - 1 == rem.vtx) 
+                    if(!visited[times[i][1] - 1]) 
                         pq.add(new Pair(times[i][1] - 1, rem.wt + times[i][2]));
         }
         return -1;
