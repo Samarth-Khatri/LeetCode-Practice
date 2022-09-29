12bb21bcfd32bc8b6f5afed09d413e466340363f
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-09-30 02:31:17 +0530

    Time: 120 ms (5.06%), Space: 69.4 MB (5.76%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -0,0 +1,29 @@
+class Solution {
+    private class Pair implements Comparable<Pair> {
+        int idx;
+        int diff;
+        
+        Pair(int idx, int diff) {
+            this.idx = idx;
+            this.diff = diff;
+        }
+        
+        public int compareTo(Pair other) {
+            if(this.diff == other.diff) return other.idx - this.idx;
+            else return other.diff - this.diff;
+        }
+    }
+    
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for(int i=0;i<arr.length;++i) {
+            pq.add(new Pair(i, Math.abs(arr[i]-x)));
+            if(pq.size() > k) pq.remove();
+        }  
+        List<Integer> ans = new ArrayList<>();
+        while(!pq.isEmpty()) 
+            ans.add(arr[pq.remove().idx]);
+        Collections.sort(ans);
+        return ans;
+    }
+}
