d263be808981ac8b3f6334b19bb2617bec19c001
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-10-18 18:48:33 +0530

    Time: 70 ms (6.94%), Space: 72.7 MB (5.01%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public String countAndSay(int n) {
+        if (n == 1) return "1";
+        if (n == 2) return "11";
+        String str = "11";
+        for (int i = 3; i <= n; ++i) {
+            int cnt = 1;
+            str += "a"; // count till 2nd last char
+            String res = "";
+            char[] array = str.toCharArray();
+            for (int j = 1; j < array.length; ++j) {
+                if (array[j] != array[j - 1]) {
+                    res += cnt;
+                    res += array[j - 1];
+                    cnt = 1;
+                } 
+                else cnt++;
+            }
+            str = res;
+        }
+        return str;
+    }
+}
